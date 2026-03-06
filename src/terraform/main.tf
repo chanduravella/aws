f641ede534844cac848d774b7d844da399cb7f2f
chandu ravella <chandu.ravella401@gmail.com>
2026-03-06 22:44:29 +0530

    import bucket

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -6,3 +6,8 @@ resource "aws_s3_object" "tf_state" {
   bucket = aws_s3_bucket.common_bucket.bucket
   key    = "tf_state"
 }
+
+import {
+  to = aws_s3_bucket.common_bucket
+  id = "common-resource-bucket"
+}
